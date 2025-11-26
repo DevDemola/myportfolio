c2d5ed178fc60e3d9d89705fd3ed7e2a4dc6b402
demmy <Omiyejuwon Ademola>
2025-11-26 21:12:39 +0100

    Fix Footer.css import case

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./footer.css";
+import "./Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-container">
-
         <div className="footer-left">
           <h3>Dev-Demmy</h3>
           <p> Certified Frontend Developer • React • JavaScript</p>
@@ -16,7 +15,6 @@ export default function Footer() {
           <a href="#skills">Skills</a>
           <a href="#contact">Contact</a>
         </div>
-
       </div>
 
       <div className="footer-bottom">
